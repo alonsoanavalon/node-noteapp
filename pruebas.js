b5068e01e78db645db00a99e50962aba43f905d8
keyzen <alonso.anavalon@gmail.com>
2021-02-15 19:00:27 -0300

    Realizado view para agregar nota

---
+++ pruebas.js
@@ -0,0 +1,6 @@
+const arrayObj = [
+    {name:'alonso', apellido:'Anavalon'},
+    {name:'Melisa', apellido:'Fuentealba'}
+]
+
+arrayObj.forEach(n => console.log(n.apellido))
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,52 @@
 const router = require('express').Router()
+const Note = require('../models/Note')
 
 router.get('/notes', (req, res) => {
-    res.send('Notas')
+    res.send('Notass')
+})
+
+router.get('/notes/add', (req, res ) => {
+    res.render('notes/new-note')
+
+})
+
+router.post('/notes/new-note', (req, res ) => {
+    const {name, description} = req.body
+    const errors = []
+
+    if (!name) errors.push({info:'No se ha ingresado un nombre'})
+    if (!description) errors.push({info:'No se ha ingresado una descripcion'})
+
+
+
+    if (errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            name,
+            description
+        }) 
+    } else {
+
+        const toCreateNote = async (name, description) => {
+            const newNote = new Note({name, description})
+            const savedNote = newNote.save()
+            return savedNote
+        }
+        toCreateNote(name, description)
+        .then(savedNote => console.log(savedNote))
+        .then(console.log('El archivo ha sido guardado con éxito'))
+        .then(res.render('notes/new-note', {
+            name,
+            success: `La nota "${name}" ha sido creada con éxito`
+          
+        }))
+        .catch(err => console.log(err))
+        
+
+    }
+
+
+    
 })
 
 
